f69832ad9a8b419e4b0ada6010e20570d3d204b4
IrinaZ <irina.zhelokhovtseva@gmail.com>
2021-01-14 17:33:09 -0800

    run pytest

--- test_create_group.py
+++ test_create_group.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import pytest, unittest, time, re
 
 class CreateGroup(unittest.TestCase):
     def setUp(self):
