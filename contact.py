b1fe650045f1c3aa0dfc91ae88bbfa6073d73dd2
IrinaZ <irina.zhelokhovtseva@gmail.com>
2021-02-03 17:01:20 -0800

    add contact file

---
+++ contact.py
@@ -0,0 +1,29 @@
+
+
+class Contact:
+    def __init__(self, firstName, middleName, lastName, title, company,
+                             address, homePhone, mobilePhone,
+                             workPhone, fax, email_1, email_2,
+                             email_3, webPage, birthMonth,
+                             aMonth, aYear, address_2, notes,
+                             birthYear):
+        self.firstName = firstName
+        self.middleName = middleName
+        self.lastName = lastName
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homePhone = homePhone
+        self.mobilePhone = mobilePhone
+        self.workPhone = workPhone
+        self.fax = fax
+        self.email_1 = email_1
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.webPage = webPage
+        self.birthMonth = birthMonth
+        self.birthYear = birthYear
+        self.aMonth = aMonth
+        self.aYear = aYear
+        self.address_2 = address_2
+        self.notes = notes
